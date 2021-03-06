acbe89ad2615364af28879d262f244e7b6765554
sleep-404 <justjeevan07@gmail.com>
2021-03-06 20:56:50 +0530

    scroll is frucked up ls! , ditching it for now and going to tiles

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -5,36 +5,33 @@ import {useScrollPosition} from "../../hooks/useScrollPosition"
 
 
 const Calendar = () => {
-  const ref = useRef(null);
-  useEffect(()=>{
-    ref.current=1;
-  },[]);
-  const showCalendar=()=>{
-      const calendar_grid=document.querySelector('.month-name');
-      // alert("hi");
-      console.log("calender",calendar_grid)
-      if(calendar_grid) calendar_grid.scrollIntoView({behavior:"smooth"})
-  }
-  showCalendar();
-  useScrollPosition(({ prevPos, currPos }) => {
-    console.log(prevPos);
-    console.log(currPos);
-    console.log(document.body.offsetTop);
-    console.log("Next",window.innerHeight-currPos.y);
-    console.log("Documnet",document.body.offsetHeight);
-    if (window.innerHeight-currPos.y >= document.body.offsetHeight){
-      console.log("bottom");
-      showCalendar();
-    }
-    else if (currPos.y > 0){
-      console.log("top");
-      showCalendar();
-    }
-    else{
-      setTimeout(()=>{ref.current=1},0);
-    }
-  }, [])
+  // const showCalendar=()=>{
+  //   const calendar_grid=document.querySelector('.day-names');
+  //   console.log("calender",calendar_grid)
+  //   if(calendar_grid) calendar_grid.scrollIntoView({behavior:"smooth"})
+  // }
+  // showCalendar();
+  // useScrollPosition(({ prevPos, currPos }) => {
+  //   if (window.innerHeight-currPos.y >= document.body.offsetHeight){
+  //     console.log("bottom");
+  //     nextMonth();
+  //     // showCalendar();
+  //   }
+  //   else if (currPos.y > 5){
+  //     console.log("top");
+  //     prevMonth();
+  //     // showCalendar();
+  //   }
+  // }, [])
   const [month, setMonth] = useState(moment());
+  // const nextMonth = () => {
+  //   let nxt_month = month.add(1,"month").clone();
+  //   setMonth(nxt_month);
+  // }
+  // const prevMonth = () => {
+  //   let prev_month = month.subtract(1,"month").clone();
+  //   setMonth(prev_month);
+  // }
   return (
     <Month value={month}/>
   )
